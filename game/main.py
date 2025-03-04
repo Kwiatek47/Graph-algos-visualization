dcbcc1618e4bac868e1f5efa4a6d3259f80dc31a
Anton <antonikwiatek47@outlook.com>
2025-03-05 00:28:58 +0100

    Menu Update 1.0

--- game/main.py
+++ game/main.py
@@ -4,34 +4,49 @@ import pygame
 pygame.init()
 pygame.font.init()
 
+# Ustawienie czcionki
 my_font = pygame.font.SysFont('Arial', 30)
-screen = pygame.display.set_mode((1280, 720))
+
+# Wymiary okna
+w, h = 900, 900
+screen = pygame.display.set_mode((w, h))
 screen.fill("white")
 clock = pygame.time.Clock()
 running = True
+coursor = pygame.mouse.set_cursor(*pygame.cursors.diamond)
 
-def func():
-        return 'Text...'
+# Menu
+def DisplayMenu():
+        pygame.draw.line(screen, (255, 0, 0), (0, 100), (w, 100), width=3)
+        
+        dfs_surface = my_font.render("DFS", True, (0, 0, 0))
+        bfs_surface = my_font.render("BFS", True, (0, 0, 0))
+        dijkstra_surface = my_font.render("Dijkstra", True, (0, 0, 0))
+        astar_surface = my_font.render("A*(star)", True, (0, 0, 0))
 
+        x = 250
+        screen.blit(dfs_surface, (x + 10, 30))  
+        screen.blit(bfs_surface, (x + 90, 30))  
+        screen.blit(dijkstra_surface, (x + 165, 30))  
+        screen.blit(astar_surface, (x + 270, 30))  
+
+def DisplayBoard():
+        # Game, board, algos visualization, etc.
+        pass
+
+# Main Loop
 while running:
-        # poll for events
-        # pygame.QUIT event means the user clicked X to close your window
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                        running = False
+                        running = False 
                 elif event.type == pygame.KEYDOWN and event.key == pygame.K_q:
                         running = False
 
-        # RENDER YOUR GAME HERE
-
-        text = func()
-        text_surface = my_font.render(text, False, (0,0,0))
-        screen.blit(text_surface, (1280 / 2, 0))
-
-        pygame.draw.lines(screen, (0, 0, 0), False, [(0, 1), (0, 3), (2, 4), (10, 200)], 2)
+        screen.fill("white")  # Czyszczenie ekranu przed rysowaniem
+        DisplayMenu()
+        DisplayBoard()
 
         pygame.display.flip()
-
         clock.tick(60)  # limits FPS to 60
 
 pygame.quit()
